2d720620bf684ebc7a8bd548a079371e4ced9226
ArmanRuhit <armanruhit09@gmail.com>
2024-06-07 21:44:47 +0600

    Intro to props

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -4,10 +4,10 @@ import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-     <div className="content">
-      <Home/>
-     </div>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
