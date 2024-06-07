de7193d5672cb9b7beaaa777fae6786692353423
ArmanRuhit <armanruhit09@gmail.com>
2024-06-07 18:10:58 +0600

    Handle dynamic values

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const like = 50;
+  const link = 'www.google.com';
   return (
     <div className="App">
      <div className="conten">
-      <h1>Hello</h1>
+      <h1>{title}</h1>
+      <p>Liked {like} times.</p>
+      {10}
+      {'hello, ninjas'}
+      <br />
+      <a href={link}>GOOGLE</a>
      </div>
     </div>
   );
