a16452eda4deb63fe605b3b22d660250e85c11ae
ArmanRuhit <armanruhit09@gmail.com>
2024-06-07 19:05:36 +0600

    Css and dynamic styling

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,22 +1,12 @@
-import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const like = 50;
-  const link = 'www.google.com';
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Home></Home>
-     <div className="conten">
-      <h1>{title}</h1>
-      <p>Liked {like} times.</p>
-      {10}
-      {'hello, ninjas'}
-      <br />
-      <a href={link}>GOOGLE</a>
+      <Navbar/>
+     <div className="content">
+      <Home/>
      </div>
     </div>
   );
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>The dojo blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                    color: 'green',
+                    backgroundColor: 'red',
+                    borderRadius: '8px'
+                }}>New Blog</a>
             </div>
         </nav>
      );
