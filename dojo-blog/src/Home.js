b9785c2686862ad844cd6969a1aaabfe8e6731da
ArmanRuhit <armanruhit09@gmail.com>
2024-06-11 01:25:10 +0600

    handle errors

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,20 +4,30 @@ import {useState, useEffect} from 'react';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [err, setIsError] = useState(null);
 
     useEffect(() => {
         setTimeout(
-            () => {fetch('http://localhost:8000/blogs')
+            () => {fetch('http://localhost:8000/blogss')
             .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch data');
+                }
                 return res.json();
             }).then((data) => {
                 setBlogs(data);
                 setIsPending(false);
-        })},1000);
+                setIsError(null);
+        }).catch(err => {
+            setIsError(err.message);
+            setIsPending(false);
+        })
+    },1000);
     }, []);
 
     return ( 
         <div className="home">
+            {err && <div>{err}</div> }
             {isPending && <div>Loading....</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs!'></BlogList>}
         </div>
